5973e42702d0848444f4bc6fb8a97431642f44ab
mosirnik <mosirnik@gmail.com>
2021-07-23 08:04:12 +0900

    Remove redundant call to DontDestroyOnLoad

--- VRGIN/Controls/Tools/MenuTool.cs
+++ VRGIN/Controls/Tools/MenuTool.cs
@@ -71,8 +71,6 @@ namespace VRGIN.Controls.Tools
             Gui.transform.localPosition = new Vector3(0, 0.05f, -0.06f);
             Gui.transform.localRotation = Quaternion.Euler(90, 0, 0);
             Gui.IsOwned = true;
-            DontDestroyOnLoad(Gui.gameObject);
-
         }
 
         protected override void OnStart()
